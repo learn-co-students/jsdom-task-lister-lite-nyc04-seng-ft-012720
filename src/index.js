87792de38acff575ec323bfbb98ee653a8507deb
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-03-11 16:22:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ submit[1]*/
 const taskform = document.querySelector("#create-task-form")
 
 taskform.addEventListener("submit", handleformsubmit)
-
+let idnum = 0
 function handleformsubmit(event){
   event.preventDefault()
 
@@ -20,8 +20,10 @@ function handleformsubmit(event){
   //const newtask = event.target.name.value;
   const newtask = form["new-task-description"].value;
   //debugger
+  
   const task = {
-    name: newtask
+    name: newtask,
+    id: idnum
   }
   
   renderOneTask(task)
@@ -31,12 +33,22 @@ function handleformsubmit(event){
 
 
 function renderOneTask(task){
+  idnum++;
   const list = document.querySelector("#tasks")
   const outerLi = document.createElement('li')
-
+  outerLi.id = task.id
   outerLi.innerHTML =`
-  <p> Label: ${task.name}<p>
+  <p> Task: ${task.name} <button type="button" id="${task.id}">Delete Me!</button></p>
   `
-
   list.append(outerLi)
 }
+
+const remo = document.querySelector("#tasks")
+
+remo.addEventListener("Delete Me!", function (){
+  badli = remo.querySelector("#id")
+  debugger
+  badli.remove()
+})
+
+
