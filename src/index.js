a4c393d65669ee3e61d33d5ec413fd30e2765552
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-03-11 15:32:46 -0400

    Me and pano made it work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+//const submit = document.querySelector(`form.submit[value="Create New Task"]`)
+//const submit = document.querySelector("#sub")
+/*const submit = document.getElementsByTagName('input')
+submit[0]
+submit[1]*/
+
+const taskform = document.querySelector("#create-task-form")
+
+taskform.addEventListener("submit", handleformsubmit)
+
+function handleformsubmit(event){
+  event.preventDefault()
+
+  const form = event.target
+
+  //const newtask = event.target.name.value;
+  const newtask = form["new-task-description"].value;
+  //debugger
+  const task = {
+    name: newtask
+  }
+  
+  renderOneTask(task)
+
+  form.reset()
+}
+
+
+function renderOneTask(task){
+  const list = document.querySelector("#tasks")
+  const outerLi = document.createElement('li')
+
+  outerLi.innerHTML =`
+  <p> Label: ${task.name}<p>
+  `
+
+  list.append(outerLi)
+}
